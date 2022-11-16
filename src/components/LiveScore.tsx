7ca6dc37210ca389b80018e15bb79937f3c19234
rbraile <roberto.braile@gmail.com>
2022-11-16 11:40:32 +0100

    add live score mock

--- src/components/LiveScore.tsx
+++ src/components/LiveScore.tsx
@@ -1,61 +1,49 @@
-import { TEAMS } from '../config/teams';
 import TeamDetails from './TeamDetails';
+// import { LIVE_SCORES, ILiveMatch } from '../config/liveScore';
+import { ILiveMatch } from '../config/liveScore';
+const LIVE_SCORES: any = [];
 
 const LiveScore = () => {
   return (
     <>
-      <div className="flex items-center bg-gray-800 rounded mt-3 mx-3">
-        <div className="flex  justify-end mb-2 Light p-2">
-          <span className="animate-ping rounded h-2 w-2 bg-liveIndicator" />
-          <span className="text-lg mx-2 font-bold px-1">LIVE</span>
-        </div>
-        <div className="flex w-full p-2  items-center">
-          <TeamDetails
-            name={TEAMS.Spain.fifaCode}
-            flag={TEAMS.Spain.flag}
-            flagClassName="h-6 w-10"
-            nameClassName="hidden xs:block"
-            wrapperClassName="flex-row-reverse"
-          />
-          <span className="px-3">2</span>
-        </div>
-        <span>-</span>
-        <div className="flex w-full p-2  items-center">
-          <span className="px-3">0</span>
-          <TeamDetails
-            name={TEAMS.Argentina.fifaCode}
-            flag={TEAMS.Argentina.flag}
-            flagClassName="h-6 w-10"
-            nameClassName="hidden xs:block"
-          />
-        </div>
-      </div>
-      <div className="flex items-center bg-gray-800 rounded mt-3 mx-3">
-        <div className="flex  justify-end mb-2 Light p-2">
-          <span className="animate-ping rounded h-2 w-2 bg-liveIndicator" />
-          <span className="text-lg mx-2 font-bold px-1">LIVE</span>
-        </div>
-        <div className="flex w-full p-2 items-center">
-          <TeamDetails
-            name={TEAMS.Spain.fifaCode}
-            flag={TEAMS.Spain.flag}
-            flagClassName="h-6 w-10"
-            wrapperClassName="flex-row-reverse"
-            nameClassName="hidden xs:block"
-          />
-          <span className="px-3">2</span>
-        </div>
-        <span>-</span>
-        <div className="flex w-full p-2 items-center">
-          <span className="px-3">0</span>
-          <TeamDetails
-            name={TEAMS.Argentina.fifaCode}
-            flag={TEAMS.Argentina.flag}
-            flagClassName="h-6 w-10"
-            nameClassName="hidden xs:block"
-          />
-        </div>
-      </div>
+      {LIVE_SCORES.length > 0 ? (
+        <>
+          {LIVE_SCORES.map((liveMatch: ILiveMatch) => (
+            <div
+              key={liveMatch.team1.fifaCode + liveMatch.team2.fifaCode}
+              className="flex items-center bg-gray-800 rounded mt-3 mx-3"
+            >
+              <div className="flex  justify-end mb-2 Light p-2">
+                <span className="animate-ping rounded h-2 w-2 bg-liveIndicator" />
+                <span className="text-lg mx-2 font-bold px-1">LIVE</span>
+              </div>
+
+              <div className="flex w-full p-2  items-center">
+                <TeamDetails
+                  name={liveMatch.team1.fifaCode}
+                  flag={liveMatch.team1.flag}
+                  flagClassName="h-6 w-10"
+                  nameClassName="hidden xs:block"
+                  wrapperClassName="flex-row-reverse"
+                />
+                <span className="px-3">{liveMatch.team2.score}</span>
+              </div>
+              <span>-</span>
+              <div className="flex w-full p-2  items-center">
+                <span className="px-3">{liveMatch.team2.score}</span>
+                <TeamDetails
+                  name={liveMatch.team2.fifaCode}
+                  flag={liveMatch.team2.flag}
+                  flagClassName="h-6 w-10"
+                  nameClassName="hidden xs:block"
+                />
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
---
+++ src/config/liveScore.ts
@@ -0,0 +1,42 @@
+import { ITeam } from './teams';
+
+interface ILiveTeam extends ITeam {
+  score: number;
+}
+
+interface ILiveMatch {
+  team1: ILiveTeam;
+  team2: ILiveTeam;
+}
+const LIVE_SCORES = [
+  {
+    team1: {
+      name: 'Spain',
+      fifaCode: 'ESP',
+      flag: '/flags/es.webp',
+      score: 0,
+    },
+    team2: {
+      name: 'Germany',
+      fifaCode: 'GER',
+      flag: '/flags/de.webp',
+      score: 0,
+    },
+  },
+  {
+    team1: {
+      name: 'Japan',
+      fifaCode: 'JPN',
+      flag: '/flags/jp.webp',
+    },
+    team2: {
+      name: 'Spain',
+      fifaCode: 'ESP',
+      flag: '/flags/es.webp',
+      score: 0,
+    },
+  },
+];
+
+export { LIVE_SCORES };
+export type { ILiveMatch };
