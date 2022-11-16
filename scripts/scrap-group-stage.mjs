f717ceea7cf04663b8fce8f501b689cb9779837d
erikbg7 <erik.bg7@gmail.com>
2022-11-16 21:29:09 +0100

    👷 Test script IV

--- scripts/scrap-group-stage.mjs
+++ scripts/scrap-group-stage.mjs
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 // TODO: Change to real URL before merging
 function scheduledGroupStageTask() {
-  fetch('https://world-cup-microapp-excems6r9-erikbg7.vercel.app/api/update/group-stage')
+  fetch('https://world-cup-microapp.vercel.app/api/group-results')
     .then((res) => res.json())
     .then((data) => console.log({ data }))
     .catch((e) => console.log({ e }));
