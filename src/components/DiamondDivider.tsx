e39d4554cdd57ced994eeace01e35a800fb032e6
erikbg7 <erik.bg7@gmail.com>
2022-11-18 15:37:31 +0100

    👍 Some more quick fixes

--- src/components/DiamondDivider.tsx
+++ src/components/DiamondDivider.tsx
@@ -1,8 +1,9 @@
+import clsx from 'clsx';
 import DiamondBullet from './DiamondBullet';
 
-const DiamondDivider = () => {
+const DiamondDivider: React.FC<{ className?: string }> = (props) => {
   return (
-    <div className="flex items-center justify-center w-full text-center">
+    <div className={clsx('flex items-center justify-center w-full text-center', props.className)}>
       <DiamondBullet size={3} />
       <div className="h-[1px] w-5 bg-teal"></div>
       <DiamondBullet size={2} />
--- src/pages/stadiums/index.tsx
+++ src/pages/stadiums/index.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import Image from 'next/image';
 import { STADIUMS, IStadium } from '../../config/stadiums';
+import DiamondDivider from '../../components/DiamondDivider';
 
 const StadiumsPage = () => {
   return (
-    <div className="p-2 flex flex-col overflow-auto">
+    <div className="flex flex-col px-3 py-6 overflow-auto">
       {STADIUMS.map((stadium: IStadium) => (
-        <div key={stadium.name} className="pb-5 border-b border-[#FDB936] mb-5 text-[#ddd]">
-          <h2 className="text-xl">{stadium.name}</h2>
-          <div className="relative mb-2 mt-2">
-            <Image src={stadium.image} alt={stadium.name} width={800} height={450} className="" />
+        <section>
+          <h2 className="text-2xl font-bold mb-3">{stadium.name}</h2>
+          <div key={stadium.name} className="p-2.5 border-b border border-qatar">
+            <div className="relative mb-2 mt-2">
+              <Image src={stadium.image} alt={stadium.name} width={800} height={450} className="" />
+            </div>
+            <p>
+              <strong>Capacity:</strong> {stadium.capacity}
+            </p>
+            <p className="my-4">
+              <strong>Location:</strong> {stadium.location}
+            </p>
+            <p className="flex">
+              <strong>Matches:</strong>
+              <ul>
+                {stadium.matches.map((match) => (
+                  <li key={stadium.name + match} className="pl-2">
+                    · {match}
+                  </li>
+                ))}
+              </ul>
+            </p>
           </div>
-          <p>
-            <strong>Capacity:</strong> {stadium.capacity}
-          </p>
-          <p>
-            <strong>Location:</strong> {stadium.location}
-          </p>
-          <h3>
-            <strong>Matches:</strong>
-          </h3>
-          <ul className="list-disc">
-            {stadium.matches.map((match) => (
-              <li key={stadium.name + match}>- {match}</li>
-            ))}
-          </ul>
-        </div>
+          <DiamondDivider className="my-8" />
+        </section>
       ))}
     </div>
   );
