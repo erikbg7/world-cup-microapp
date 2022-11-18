280317b486da019e3709f2addc4363d2f506cee6
erikbg7 <erik.bg7@gmail.com>
2022-11-18 10:00:59 +0100

    💄 NavBar redesign

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,8 +16,8 @@ const SECTIONS: ISection[] = [
 
 const NavBar = () => {
   return (
-    <nav className="top-0 left-0 right-0 z-10 bg-[#0061a2]">
-      <ul className="flex justify-center h-20 sm:px-20">
+    <nav className="top-0 left-0 right-0 z-10 border overflow-scroll border-b-qatar">
+      <ul className="flex items-center h-20">
         {SECTIONS.map((section) => (
           <NavBarItem key={section.id} {...section} />
         ))}
@@ -31,13 +31,13 @@ const NavBarItem: React.FC<ISection> = (props) => {
   const isActive = router.pathname === props.path;
 
   return (
-    <li className="flex flex-1 text-lg text-gray-200 font-semibold">
+    <li className="flex flex-1 text-md px-3">
       <Link
         href={props.path}
         className={clsx(
-          'flex justify-center items-center',
-          'w-full h-full hover:bg-gray-200/20',
-          isActive && 'border-b-4 border-yellow-500 text-white'
+          'border border-qatar p-2',
+          'w-full h-[50%] hover:cursor-pointer',
+          isActive && 'active-nav-link'
         )}
       >
         {props.name}
