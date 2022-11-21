f7d334bd134d5c2ce6c389e4a78edf327f7fba02
robot <akshay.pathare@nagarro.com>
2022-11-21 16:17:46 +0530

    fixed the timezone to display the correct match time

--- src/config/liveScore.ts
+++ src/config/liveScore.ts
@@ -29,7 +29,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group A',
-    time: '1668988800000',
+    time: '1669046400000',
     team1: TEAMS.Senegal,
     team2: TEAMS.Netherlands,
     scores: ['0', '0'],
@@ -37,7 +37,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group B',
-    time: '1668988800000',
+    time: '1668992400000',
     team1: TEAMS.England,
     team2: TEAMS.Iran,
     scores: ['0', '0'],
@@ -45,7 +45,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group B',
-    time: '1669075200000',
+    time: '1669057200000',
     team1: TEAMS.USA,
     team2: TEAMS.Wales,
     scores: ['0', '0'],
@@ -53,7 +53,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group C',
-    time: '1669075200000',
+    time: '1669111200000',
     team1: TEAMS.Argentina,
     team2: TEAMS.SaudiArabia,
     scores: ['0', '0'],
@@ -61,7 +61,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group C',
-    time: '1669075200000',
+    time: '1669132800000',
     team1: TEAMS.Mexico,
     team2: TEAMS.Poland,
     scores: ['0', '0'],
@@ -69,7 +69,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group D',
-    time: '1669075200000',
+    time: '1669122000000',
     team1: TEAMS.Denmark,
     team2: TEAMS.Tunisia,
     scores: ['0', '0'],
@@ -77,7 +77,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group D',
-    time: '1669161600000',
+    time: '1669143600000',
     team1: TEAMS.France,
     team2: TEAMS.Australia,
     scores: ['0', '0'],
@@ -85,7 +85,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group E',
-    time: '1669161600000',
+    time: '1669208400000',
     team1: TEAMS.Germany,
     team2: TEAMS.Japan,
     scores: ['0', '0'],
@@ -93,7 +93,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group E',
-    time: '1669161600000',
+    time: '1669219200000',
     team1: TEAMS.Spain,
     team2: TEAMS.CostaRica,
     scores: ['0', '0'],
@@ -101,7 +101,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group F',
-    time: '1669161600000',
+    time: '1669197600000',
     team1: TEAMS.Morocco,
     team2: TEAMS.Croatia,
     scores: ['0', '0'],
@@ -109,7 +109,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group F',
-    time: '1669248000000',
+    time: '1669230000000',
     team1: TEAMS.Belgium,
     team2: TEAMS.Canada,
     scores: ['0', '0'],
@@ -117,7 +117,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group G',
-    time: '1669248000000',
+    time: '1669284000000',
     team1: TEAMS.Switzerland,
     team2: TEAMS.Cameroon,
     scores: ['0', '0'],
@@ -125,7 +125,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group G',
-    time: '1669334400000',
+    time: '1669316400000',
     team1: TEAMS.Brazil,
     team2: TEAMS.Serbia,
     scores: ['0', '0'],
@@ -133,7 +133,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group H',
-    time: '1669248000000',
+    time: '1669294800000',
     team1: TEAMS.Uruguay,
     team2: TEAMS.SouthKorea,
     scores: ['0', '0'],
@@ -141,7 +141,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group H',
-    time: '1669248000000',
+    time: '1669305600000',
     team1: TEAMS.Portugal,
     team2: TEAMS.Ghana,
     scores: ['0', '0'],
@@ -149,7 +149,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group H',
-    time: '1669593600000',
+    time: '1669640400000',
     team1: TEAMS.SouthKorea,
     team2: TEAMS.Ghana,
     scores: ['0', '0'],
@@ -157,7 +157,7 @@ const LIVE_SCORES: ILiveMatch[] = [
   },
   {
     group: 'Group H',
-    time: '1669680000000',
+    time: '1669662000000',
     team1: TEAMS.Portugal,
     team2: TEAMS.Uruguay,
     scores: ['0', '0'],
