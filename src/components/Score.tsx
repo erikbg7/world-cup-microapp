273abec5767824e262fcfeae44f4582f1ecf1c55
erikbg7 <erik.bg7@gmail.com>
2022-11-23 16:09:41 +0100

    💄 Fix score styles in modal

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -4,14 +4,16 @@ import React from 'react';
 interface Props {
   score: string;
   isWinner: boolean;
+  className?: string;
 }
 
-const Score: React.FC<Props> = ({ score, isWinner }) => {
+const Score: React.FC<Props> = ({ score, isWinner, className }) => {
   return (
     <span
       className={clsx(
         `w-7 h-7 bg-white rotate-45 flex items-center justify-center border-r border-t mr-4 `,
-        isWinner ? `border-teal` : `border-red`
+        isWinner ? `border-teal` : `border-red`,
+        className
       )}
     >
       <strong className="-rotate-45">{score}</strong>
